e2bfc855966233be9f9097108a6512b8065993f9
GBelther <belthergabriel@gmail.com>
2021-12-14 22:42:22 -0300

    feature: componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header } from "./styles";
+import { Container, Header, CarImages } from "./styles";
 
 export function CarDetails() {
   return (
@@ -10,6 +11,14 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://w7.pngwing.com/pngs/444/585/png-transparent-2018-audi-tt-rs-car-audi-rs5-coupe-audi-compact-car-car-performance-car.png",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
