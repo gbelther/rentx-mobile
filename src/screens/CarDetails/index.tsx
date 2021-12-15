572abf8c5b1a0c1f9aa17559df42bde951d5ecce
GBelther <belthergabriel@gmail.com>
2021-12-14 22:56:47 -0300

    feature: continuação da página de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,7 +3,20 @@ import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -19,6 +32,27 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lambirghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          laboriosam ad, omnis dicta, animi ratione, temporibus neque pariatur
+          et architecto eligendi labore rem voluptate similique quasi!
+          Architecto quam omnis iusto?
+        </About>
+      </Content>
     </Container>
   );
 }
