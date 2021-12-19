7758d884bf9cf203cdf33a70fe0750365a831334
GBelther <belthergabriel@gmail.com>
2021-12-19 16:44:18 -0300

    feature: exibição dos carros agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { FlatList, StatusBar } from "react-native";
+import {
+  NavigationProp,
+  ParamListBase,
+  useNavigation,
+} from "@react-navigation/native";
+import { useTheme } from "styled-components";
+
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
 
 import { CarDTO } from "../../dtos/CarDTO";
 import { api } from "../../services/api";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+} from "./styles";
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const theme = useTheme();
+  const { navigate, goBack }: NavigationProp<ParamListBase> = useNavigation();
+
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+
+  function handleBack() {
+    goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -22,7 +54,42 @@ export function MyCars() {
         setLoading(false);
       }
     }
+
+    fetchCars();
   }, []);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {"\n"}
+          data de início e {"\n"}
+          fim do aluguel
+        </Title>
+
+        <SubTitle>Conforto, segurança e praticidade</SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+
+        <FlatList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => <Car data={item.car} />}
+        />
+      </Content>
+    </Container>
+  );
 }
