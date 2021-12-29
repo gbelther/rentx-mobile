dcba29efd55c69248a41116fb74896277023f6ec
GBelther <belthergabriel@gmail.com>
2021-12-29 20:46:51 -0300

    feature: tabela de carros

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -6,19 +6,17 @@ import { Splash } from "../screens/Splash";
 import { SignIn } from "../screens/SignIn";
 import { SignUpFirstStep } from "../screens/SignUp/SignUpFirstStep";
 import { SignUpSecondStep } from "../screens/SignUp/SignUpSecondStep";
-import { Profile } from "../screens/Profile";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
   return (
     <Navigator
-      initialRouteName="Profile"
+      initialRouteName="Splash"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Screen name="Profile" component={Profile} />
       <Screen name="Splash" component={Splash} />
       <Screen name="SignIn" component={SignIn} />
       <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
